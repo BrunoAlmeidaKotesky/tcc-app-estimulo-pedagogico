c8c3cb62485182fadc00fc9da627e0a5235ac71c
Bruno Kotesky <bruno.kotesky@trentim.com>
2023-08-24 21:13:16 -0300

    Logica exercoses

--- src/app/api/get-exercises/route.ts
+++ src/app/api/get-exercises/route.ts
@@ -1,6 +1,68 @@
 import { getErrorResponse } from "@/lib/helpers";
 import { prisma } from "@/lib/prisma";
+import { Exercise } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+async function getInitialExercises(
+    childId: string,
+    numberOfAnsweredEx: number,
+    answeredExerciseIds: string[]
+): Promise<Exercise[]> {
+
+    // Obtendo os assuntos dos exercícios já respondidos
+    const answeredSubjects = await prisma.answeredExercise.findMany({
+        where: {
+            childId,
+        },
+        select: {
+            exercise: {
+                select: {
+                    subjectId: true
+                }
+            }
+        }
+    });
+    const answeredSubjectsIds = answeredSubjects.map(subject => subject?.exercise?.subjectId);
+
+    const exercisesToGet = await prisma.exercise.findMany({
+        where: {
+            NOT: {
+                id: {
+                    in: answeredExerciseIds
+                },
+                subjectId: {
+                    in: answeredSubjectsIds
+                }
+            },
+            difficulty: {
+                // Peso 1 = fácil
+                weight: 1
+            }
+        },
+        take: 10 - numberOfAnsweredEx,
+    });
+
+    if (exercisesToGet.length < (10 - numberOfAnsweredEx)) {
+        // Se não encontrarmos exercícios suficientes de diferentes assuntos, 
+        // pegue mais exercícios de assuntos já respondidos para preencher a lacuna
+        const additionalExercises = await prisma.exercise.findMany({
+            where: {
+                NOT: {
+                    id: {
+                        in: answeredExerciseIds
+                    }
+                },
+                difficulty: {
+                    weight: 1
+                }
+            },
+            take: (10 - numberOfAnsweredEx) - exercisesToGet.length,
+        });
+        exercisesToGet.push(...additionalExercises);
+    }
+
+    return exercisesToGet;
+}
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
     try {
@@ -9,27 +71,19 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
         if (!childId)
             return getErrorResponse(401, "Você não está logado, por favor forneça um token de acesso.");
         const answeredExercises = await prisma.answeredExercise.findMany({
-            where: {
-                childId
-            }
+            where: { childId }
         });
+
         const numberOfAnsweredEx = answeredExercises.length;
-        if(numberOfAnsweredEx <= 10) {
-            const exercisesToGet = await prisma.exercise.findMany({
-                take: 10 - numberOfAnsweredEx,
-                /*where: {
-                    difficulty: 
-                }*/
-            });
+        const answeredExerciseIds = answeredExercises.map(exercise => exercise.exerciseId);
+        if (numberOfAnsweredEx < 10) {
+            const exercises = await getInitialExercises(childId, numberOfAnsweredEx, answeredExerciseIds);
+
         }
-        
+        return new NextResponse(JSON.stringify({}), { 
+            status: 200, headers: { "Content-Type": "application/json" }
+        });
     } catch (e: any) {
         return getErrorResponse(500, e.message);
     }
-}
-
-interface ExerciseBody {
-    exerciseId: string;
-    childId: string;
-    answerId: string;
 }
